5b320f90daafdf9486901b75b113cc723c27183c
Swastik Tripathi <swastiktripathi.space@gmail.com>
2025-06-09 05:00:11 +0000

    1

--- .next/static/chunks/src_eadf5257._.js
+++ .next/static/chunks/src_eadf5257._.js
@@ -1034,7 +1034,7 @@ function PublicNavbar({ activeLink }) {
                                     className: "shadow-md rounded-full h-9 px-4 text-sm",
                                     children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$compiled$2f$react$2f$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$client$2f$app$2d$dir$2f$link$2e$js__$5b$app$2d$client$5d$__$28$ecmascript$29$__["default"], {
                                         href: "/auth?action=signup",
-                                        children: "Get Started Free"
+                                        children: "Get Started Free."
                                     }, void 0, false, {
                                         fileName: "[project]/src/components/layout/PublicNavbar.tsx",
                                         lineNumber: 205,
